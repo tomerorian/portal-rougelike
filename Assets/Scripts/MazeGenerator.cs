85a6645b0f8e2a10e1a0fd3cea18358c3ddcfeb7
Tomer <gogofo@gmail.com>
2020-11-12 14:38:48 +0200

    add plain tiles sprites

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [SerializeField] Tilemap layoutTileMap;
+    [SerializeField] Tile tile;
+
+    void Start()
+    {
+        Vector3Int tilePos = Vector3Int.zero;
+
+        for (int i = 0; i < 10; i++)
+        {
+            layoutTileMap.SetTile(tilePos, tile);
+            tilePos.x += 1;
+        }
+    }
+}
