4daa4fc43df81c6ca82f6a90c1f9276ae28bab63
Tomer <gogofo@gmail.com>
2020-11-12 15:01:16 +0200

    add a very stupid player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(rb.position * 1.1f);
+    }
+}
