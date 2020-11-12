19a74fa03f73fa561de3ed749bfa0f4020354a47
Tomer <gogofo@gmail.com>
2020-11-12 17:00:16 +0200

    import follow camera

---
+++ Assets/Scripts/Camera/AttachVirtualCameraToPlayer.cs
@@ -0,0 +1,12 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachVirtualCameraToPlayer : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<ICinemachineCamera>().Follow = FindObjectOfType<PlayerController>().transform;
+    }
+}
---
+++ Assets/Scripts/Camera/SetCinemacineSizeAccordingToCamera.cs
@@ -0,0 +1,23 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCinemacineSizeAccordingToCamera : MonoBehaviour
+{
+    [SerializeField] Camera referenceCamera = null;
+
+    int updateAttempts = 5;
+
+    private void Update()
+    {
+        GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = referenceCamera.orthographicSize;
+
+        updateAttempts--;
+
+        if (updateAttempts == 0)
+        {
+            Destroy(this);
+        }
+    }
+}
