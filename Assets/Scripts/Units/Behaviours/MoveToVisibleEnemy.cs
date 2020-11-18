ff3c3f1cdb282e14e926bafa535303df19205dfa
Tomer <gogofo@gmail.com>
2020-11-18 21:26:06 +0200

    detect player

--- Assets/Scripts/Units/Behaviours/MoveToVisibleEnemy.cs
+++ Assets/Scripts/Units/Behaviours/MoveToVisibleEnemy.cs
@@ -7,6 +7,8 @@ public class MoveToVisibleEnemy : TurnBasedBehaviour
 {
     [Header("Config")]
     [SerializeField] float visionRange = 5f;
+    [SerializeField] LayerMask enemyLayers = 0;
+    [SerializeField] LayerMask visiionBlockingLayers = 0;
 
     protected override TurnBasedUnit.BehaviourPriority GetPriority()
     {
@@ -22,12 +24,31 @@ public class MoveToVisibleEnemy : TurnBasedBehaviour
         if (enemy)
         {
             // TODO: Attack enemy
-            yield break;
+            print("Found enemy! " + enemy.name);
+            DidAction = true;
         }
+
+        yield break;
     }
 
     private MazeUnit FindVisibleEnemy()
     {
+        // Find all enemies within range
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, visionRange, Vector2.zero, 0, enemyLayers);
+
+        if (hits.Length == 0) { return null; }
+
+        // Find first visible target
+        foreach (RaycastHit2D hit in hits)
+        {
+            RaycastHit2D visibleHit = Physics2D.Raycast(transform.position, hit.transform.position - transform.position, float.MaxValue, enemyLayers | visiionBlockingLayers);
+
+            if (hit.collider == visibleHit.collider)
+            {
+                return hit.collider.GetComponent<MazeUnit>();
+            }
+        }
+
         return null;
     }
 }
