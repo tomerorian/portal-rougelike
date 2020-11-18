edef8a1d3629fea726536a28a1824b7d23ac002b
Tomer <gogofo@gmail.com>
2020-11-18 20:28:04 +0200

    WIP: move to visible
    started and realized the bahaviours need a bit more refactoring

---
+++ Assets/Scripts/Units/Behaviours/MoveToVisibleEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToVisibleEnemy : TurnBasedBehaviour
+{
+    [Header("Config")]
+    [SerializeField] float visionRange = 5f;
+
+    protected override TurnBasedUnit.BehaviourPriority GetPriority()
+    {
+        return TurnBasedUnit.BehaviourPriority.MOVEMENT;
+    }
+
+    public override IEnumerator TakeTurn()
+    {
+        MazeUnit enemy = FindVisibleEnemy();
+
+        if (enemy)
+        {
+            // TODO: Attack enemy
+            yield return null;
+        }
+    }
+
+    private MazeUnit FindVisibleEnemy()
+    {
+        return null;
+    }
+}
