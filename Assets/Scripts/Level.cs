4ec5ad7facb5dbd6c776034ebc7d5dec53bd47fb
Tomer <gogofo@gmail.com>
2020-11-18 14:21:27 +0200

    make units take turn with a delay from the player

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -9,6 +9,7 @@ public class Level : MonoBehaviour
     public static Level Instance { get; private set; }
 
     bool isPlayerTurn = false;
+    bool hasUnitsRoutineRunning = false;
     List<TurnBasedUnit> units = new List<TurnBasedUnit>();
     List<TurnBasedUnit> removedUnits = new List<TurnBasedUnit>();
 
@@ -24,9 +25,17 @@ public class Level : MonoBehaviour
         InitGame();
     }
 
+    private void Update()
+    {
+        if (IsPlayerTurn() || hasUnitsRoutineRunning) { return; }
+
+        StartCoroutine(DoUnitsTurn());
+    }
+
     private void InitGame()
     {
         isPlayerTurn = true;
+        hasUnitsRoutineRunning = false;
         units.Clear();
         removedUnits.Clear();
     }
@@ -54,12 +63,14 @@ public class Level : MonoBehaviour
     public void OnPlayerTookAction()
     {
         isPlayerTurn = false;
-
-        StartCoroutine(DoEnemyTurn());
     }
 
-    private IEnumerator DoEnemyTurn()
+    private IEnumerator DoUnitsTurn()
     {
+        hasUnitsRoutineRunning = true;
+
+        yield return new WaitForFixedUpdate();
+
         List<Coroutine> turnRoutines = new List<Coroutine>();
 
         foreach (TurnBasedUnit unit in units)
@@ -74,6 +85,7 @@ public class Level : MonoBehaviour
 
         RemoveUnits();
 
+        hasUnitsRoutineRunning = false;
         isPlayerTurn = true;
     }
 
