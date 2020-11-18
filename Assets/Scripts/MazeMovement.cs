cef89e96350451e13a4b777fa0fce7dc48f8df22
Tomer <gogofo@gmail.com>
2020-11-18 11:38:29 +0200

    make enemy block players path

--- Assets/Scripts/MazeMovement.cs
+++ Assets/Scripts/MazeMovement.cs
@@ -6,6 +6,10 @@ public class MazeMovement : GridMovement
 {
     Maze maze = null;
 
+    [Header("Config")]
+    [SerializeField] LayerMask blockingLayers;
+
+
     private void Start()
     {
         maze = Maze.Instance;
@@ -13,12 +17,24 @@ public class MazeMovement : GridMovement
 
     public bool AttemptMove(Vector2Int direction)
     {
-        if (maze.CanMoveTo(GetPosition() + direction))
-        {
-            Move(direction);
-            return true;
-        }
+        Vector2Int targetPos = GetPosition() + direction;
 
-        return false;
+        if (!maze.CanMoveTo(targetPos)) { return false; }
+
+        if (IsSomethingBlocking(direction)) { return false; }
+
+        Move(direction);
+
+        return true;
+    }
+
+    private bool IsSomethingBlocking(Vector2Int direction)
+    {
+        // Assuming x and y size are identical
+        float cellSize = grid.cellSize.x;
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction, cellSize, blockingLayers);
+
+        return hits.Length > 0;
     }
 }
