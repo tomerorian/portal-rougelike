2dca9cb89523169a2cddccf39c39e5649a1e0af3
Tomer <gogofo@gmail.com>
2020-11-22 01:58:30 +0200

    display hearths with grid layout

--- Assets/Scripts/UI/HUDHealthDisplay.cs
+++ Assets/Scripts/UI/HUDHealthDisplay.cs
@@ -11,22 +11,9 @@ public class HUDHealthDisplay : HealthDisplay<Image>
     protected override void SetComponentWithSprite(Image component, Sprite sprite)
     {
         component.sprite = sprite;
-        component.SetNativeSize();
     }
 
     protected override void SetContainerPositionByIndex(GameObject container, int index)
     {
-        float containerX = transform.position.x + index * gapBetweenHearthCenters * transform.localScale.x;
-        container.transform.position = new Vector2(containerX, transform.position.y);
     }
-
-#if UNITY_EDITOR
-    private void OnDrawGizmos()
-    {
-        GUIStyle style = new GUIStyle();
-        style.fontSize = 30;
-        style.normal.textColor = Color.black;
-        UnityEditor.Handles.Label(transform.position, "Health Display", style);
-    }
-#endif
 }
