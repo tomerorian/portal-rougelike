7d941b2ff698ea9e7e3503e76b176ac6e0ecf7ee
Tomer <gogofo@gmail.com>
2020-11-22 14:51:49 +0200

    player can kill units

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -56,6 +56,7 @@ public class Level : MonoBehaviour
     {
         isPlayerTurn = false;
 
+        RemoveUnits();
         StartCoroutine(DoUnitsTurn());
     }
 
---
+++ Assets/Scripts/Units/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] Health health = null;
+
+    private void Start()
+    {
+        health.onDeath += OnDeath;
+    }
+
+    private void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+}
